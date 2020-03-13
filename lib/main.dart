1b7edba622c68b299ee4a0b66c502ac037b19857
Sanket Naik <sanketnaik99@gmail.com>
2020-03-13 12:57:12 +0530

    Firebase Auth and Firestore Functions Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/providers/auth_bloc.dart';
 import 'package:flutter_practice/router.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   FluroRouter.setupRouter();
-  runApp(MaterialApp(
-    title: 'Doctor\'s Prescription',
-    initialRoute: 'login',
-    onGenerateRoute: FluroRouter.router.generator,
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthBloc>.value(value: AuthBloc()),
+      ],
+      child: MaterialApp(
+        title: 'Doctor\'s Prescription',
+        initialRoute: 'login',
+        onGenerateRoute: FluroRouter.router.generator,
+      ),
+    );
+  }
 }
